804bb37272c2735bffb032e0ac5c7b7ea192f2e6
Mikko Suniala <mikko.suniala@iki.fi>
2016-10-12 19:38:58 +0300

    Fix type parameters in test_pipeline.sh

--- scripts/test_pipeline.sh
+++ scripts/test_pipeline.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-echo "Device 18448 should pass, location should be as 6 eteiskomero"
-curl -d "key=asdf&type=1&id=18448&value=34.56" http://localhost:8080
+echo "Device 18448/1 should pass, location should be as 6 eteiskomero"
+curl -d "key=asdf&type_id=1&id=18448&value=34.56" http://localhost:8080
+echo
 echo
 
 echo "Device 54321 should not pass"
-curl -d "key=asdf&type=1&id=54321&value=1234.56" http://localhost:8080
+curl -d "key=asdf&type_id=1&id=54321&value=1234.56" http://localhost:8080
 echo
 
 echo "Device 11002 should pass, location should be lämminvesivaraaja"
-curl -d "key=asdf&type=1&id=11002&value=12" http://localhost:8080
+curl -d "key=asdf&type_id=1&id=11002&value=12" http://localhost:8080
 echo
 
 echo "Device 11002 should not pass due to invalid key"
-curl -d "key=qwer&type=1&id=11002&value=12" http://localhost:8080
+curl -d "key=qwer&type_id=1&id=11002&value=12" http://localhost:8080
 echo
